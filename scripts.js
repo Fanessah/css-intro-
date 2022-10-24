6892f03eb331fec6989dbc97cf9597f60a4c0b86
Fanessa Hilaire <fan@Fanessas-MacBook-Air.local>
2022-10-24 15:17:22 -0400

    update

---
+++ scripts.js
@@ -0,0 +1,24 @@
+const family = [
+    {
+    dad: true,
+    name: "Joe",
+    Age: 32 
+},
+{
+    mom: true,
+    name: "Nancy",
+    Age: 30
+}
+
+]
+console.log('hey clas this is a family');
+
+document.getElementById('contact').style.color = 'orange'
+
+const handleColorChange = () =>{
+console.log ('changing color')
+}
+
+const handleAlert = () => {
+    alert ('Alerting the class')
+}
